84c8acf67d6d83eb15d5919d1a352f97570962e1
dres0 <humberto.ferreiray@gmail.com>
2022-06-28 22:12:29 -0400

    Updated

--- carts.rb
+++ carts.rb
@@ -1,44 +1,29 @@
-def to_controller1
-    response = EXPRESS_GATEWAY.setup_purchase(price,
-    ip: request.remote_ip,
-    return_url: process_paypal_payment_cart_url,
-    cancel_return_url: root_url,
-    allow_guest_checkout: true,
-    currency: "USD"
-    )
+class Car < ApplocationRecord 
+    def payment_method
+        payment_method = PaymentMethod.find_by(code: "PEC")
+        Payment.create(
+            order_id: order.id,
+            payment_method_id: payment_method.id,
+            state: "processing",
+            total: order.total,
+            token: response.token
+        )
+    end
 
-    payment_method = PaymentMethod.find_by(code: "PEC")
-    Payment.create(
-        order_id: order.id,
-        payment_method_id: payment_method.id,
-        state: "processing",
-        total: order.total,
-        token: response.token
-    )
-end
+    def process_paypal_payment
+        response = EXPRESS_GATEWAY.purchase(price, express_purchase_options)
+        if response.success?
+            payment = Payment.find_by(token: response.token)
+            order = payment.order
 
-def to_controller2
-    if response.success?
-        payment = Payment.find_by(token: response.token)
-        order = payment.order
+            #update object states   
+            payment.state = "completed"
+            order.state = "completed"
 
-        #update object states   
-        payment.state = "completed"
-        order.state = "completed"
-
-        ActiveRecord::Base.transaction do
-            order.save!
-            payment.save!
+            ActiveRecord::Base.transaction do
+                order.save!
+                payment.save!
+            end
         end
     end
-end
-
-def to_controller3
-    express_purchase_options =
-    {
-        ip: request.remote_ip,
-        token: params[:token],
-        payer_id: details.payer_id,
-        currency: "USD"
-    }
 end
--- carts_controller.rb
+++ carts_controller.rb
@@ -1,6 +1,8 @@
 class CartsController < ApplicationController
     before_action :authenticate_user!
-    
+    before_action :cart, only: [:show, :update]
+
+
     def update
         product = params[:cart][:product_id]
         quantity = params[:cart][:quantity]
@@ -15,19 +17,31 @@ class CartsController < ApplicationController
     end
     
     def pay_with_paypal
-        order = Order.find(params[:cart][:order_id])
-    
+        @order = Order.find(params[:cart][:order_id])
         #price must be in cents
         price = order.total * 100
-    
+        response = EXPRESS_GATEWAY.setup_purchase(price,
+            ip: request.remote_ip,
+            return_url: process_paypal_payment_cart_url,
+            cancel_return_url: root_url,
+            allow_guest_checkout: true,
+            currency: "USD"
+        )
+        render json: { pay_with_paypal: @order.payment_method}
         redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
     end
     
     def process_paypal_payment
+        @order = Order.find(params[:cart][:order_id])
         details = EXPRESS_GATEWAY.details_for(params[:token])
-        
+        express_purchase_options =
+        {
+            ip: request.remote_ip,
+            token: params[:token],
+            payer_id: details.payer_id,
+            currency: "USD"
+        }
         price = details.params["order_total"].to_d * 100
-        
-        response = EXPRESS_GATEWAY.purchase(price, express_purchase_options)
+        render json: { process_paypal_payment: @order.process_paypal_payment}
     end
 end        
